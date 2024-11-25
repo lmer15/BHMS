120b6ccdc382721c98e57015032087479b3b94b6
lmer15 <raponelmer15@gmail.com>
2024-11-26 06:42:01 +0800

    PHASE 4.2

--- bhms.sql
+++ bhms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2024 at 06:40 AM
+-- Generation Time: Nov 25, 2024 at 11:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `aminities_services`
+--
+
+CREATE TABLE `aminities_services` (
+  `amiser_id` int(20) NOT NULL,
+  `amiser_title` varchar(50) NOT NULL,
+  `amiser_desc` text NOT NULL,
+  `amiser_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `management_account`
+--
+
+CREATE TABLE `management_account` (
+  `manage_ID` int(20) NOT NULL,
+  `manage_email` varchar(50) NOT NULL,
+  `manage_password` varchar(200) NOT NULL,
+  `manage_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `management_account`
+--
+
+INSERT INTO `management_account` (`manage_ID`, `manage_email`, `manage_password`, `manage_status`) VALUES
+(1, 'management@gmail.com', 'management', 'manager');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `room_id` int(20) NOT NULL,
+  `room_image` varchar(200) NOT NULL,
+  `room_number` varchar(20) NOT NULL,
+  `room_type` varchar(20) NOT NULL,
+  `room_size` int(100) NOT NULL,
+  `room_aminities` text NOT NULL,
+  `room_utilities` text NOT NULL,
+  `rental_rates` decimal(50,2) NOT NULL,
+  `room_payfre` varchar(100) NOT NULL,
+  `room_deporate` decimal(50,2) NOT NULL,
+  `room_status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tenant_accounts`
 --
 
@@ -33,6 +86,7 @@ CREATE TABLE `tenant_accounts` (
   `lname` varchar(50) NOT NULL,
   `gender` varchar(20) NOT NULL,
   `email_address` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `contact_number` varchar(20) NOT NULL,
   `password` varchar(200) NOT NULL,
   `religion` varchar(100) NOT NULL,
@@ -45,9 +99,8 @@ CREATE TABLE `tenant_accounts` (
 -- Dumping data for table `tenant_accounts`
 --
 
-INSERT INTO `tenant_accounts` (`tc_id`, `fname`, `lname`, `gender`, `email_address`, `contact_number`, `password`, `religion`, `nationality`, `occupation`, `status`) VALUES
-(24, 'SANDRIA', 'CADUSALE', 'male', 'sandriacadusale@gmail.com', '09438284163', '$2y$10$dxXXXvWl9y9K3/zIwLuVZuQ5HEhm.4j29TlKyvLVl64KNoQ7bbzP.', 'Catholic', 'Filipino', 'Businesswoman', 'reservee'),
-(26, 'Elmer', ' Rapon', 'male', 'raponelmer15@gmail.com', '09107998581', '$2y$10$z.khVpkqG8NaIUBkC2LAQeNuxXK0BNPHeJwIZdKVITFrGUDzvwDnW', 'Roman Catholic', 'Filipino Citizen', 'IT', 'reservee');
+INSERT INTO `tenant_accounts` (`tc_id`, `fname`, `lname`, `gender`, `email_address`, `username`, `contact_number`, `password`, `religion`, `nationality`, `occupation`, `status`) VALUES
+(31, 'SANDRIA', 'CADUSALE', '', 'sandriacadusale@gmail.com', 'lmer15', '09438284163', '$2y$10$2nUMkDSGLDgpl627UDqQYeDd7VoA/6AKWA3KW21s7uDizniysxEaa', '', '', '', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -65,6 +118,24 @@ CREATE TABLE `tenant_profile` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `aminities_services`
+--
+ALTER TABLE `aminities_services`
+  ADD PRIMARY KEY (`amiser_id`);
+
+--
+-- Indexes for table `management_account`
+--
+ALTER TABLE `management_account`
+  ADD PRIMARY KEY (`manage_ID`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`room_id`);
 
 --
 -- Indexes for table `tenant_accounts`
@@ -85,10 +156,28 @@ ALTER TABLE `tenant_profile`
 --
 
 --
+-- AUTO_INCREMENT for table `aminities_services`
+--
+ALTER TABLE `aminities_services`
+  MODIFY `amiser_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `management_account`
+--
+ALTER TABLE `management_account`
+  MODIFY `manage_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `room_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tenant_accounts`
 --
 ALTER TABLE `tenant_accounts`
-  MODIFY `tc_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `tc_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `tenant_profile`
